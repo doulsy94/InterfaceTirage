73b6047711e91678776a97c1f6ac9d51cead2557
Abdoulaye Sy <doulsy2021@gmail.com>
2022-09-09 20:08:49 +0000

    Le projet

--- interfacetirage/src/app/app.component.spec.ts
+++ interfacetirage/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'interfacetirage'`, () => {
+  it(`should have as title 'interfaceTirage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('interfacetirage');
+    expect(app.title).toEqual('interfaceTirage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('interfacetirage app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('interfaceTirage app is running!');
   });
 });
